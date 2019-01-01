0bfb991e1a347eeae9fc7362d5f94ad57bdb21ac
Omid Moradzadeh <omdroid@omdroids-mac-mini.local>
2019-01-01 11:53:13 +0330

    Initial Commit

---
+++ ToDoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoey
+//
+//  Created by omDroid on 1/1/19.
+//  Copyright © 2019 iomDroid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
